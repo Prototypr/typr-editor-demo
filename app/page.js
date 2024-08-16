7cf0dddb4c026f29652aacd4117154ef8c547e9b
Graeme Fulton <graeme@prototypr.io>
2024-08-16 17:44:59 +0100

    metadata

--- app/page.js
+++ app/page.js
@@ -6,16 +6,6 @@ import Head from 'next/head';
 export default function EditorPage() {
   return (
     <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.openGraph.title} />
-        <meta property="og:description" content={metadata.openGraph.description} />
-        <meta property="og:image" content={metadata.openGraph.images[0].url} />
-        <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
-        <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
-        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
-      </Head>
       <Tiptypr
         requireLogin={false}
         components={{
