8fced525f3598479e1df26269d38247409ccb1d2
Graeme Fulton <graeme@prototypr.io>
2024-08-22 02:22:00 +0100

    meta

--- app/demo/page.js
+++ app/demo/page.js
@@ -1,25 +1,6 @@
 import React, { Suspense } from "react";
 import DemoPageContent from "./PageContent";
 
-export const metadata = {
-  title: "Typr Editor - Open-Source Writing Tool by Prototypr",
-  description:
-    "A customizable WYSIWYG editor with publishing flows and user state management for React.js. Built with Tiptap and ProseMirror.",
-  openGraph: {
-    title: "Typr Editor - Open-Source Writing Tool by Prototypr",
-    description:
-      "A customizable WYSIWYG editor with publishing flows and user state management for React.js. Built with Tiptap and ProseMirror.",
-    images: [
-      {
-        url: `https://typr.dev/typr-og.png`,
-        width: 1200,
-        height: 630,
-        alt: "Tiptypr Editor Preview",
-      },
-    ],
-  },
-};
-
 
 
 export default function DemoPage() {
